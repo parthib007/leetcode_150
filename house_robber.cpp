3461cddfc90c8c7511ff44bb9f69be2187fc88c9
parthib_007 <98155243+parthib007@users.noreply.github.com>
2025-10-08 03:03:54 +0530

    Update house_robber.cpp

--- house_robber.cpp
+++ house_robber.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n = nums.size();
+        if(n==1)    return nums[0]
         vector<int> dp(n);
         dp[0] = nums[0]; // when at 0th house, take that money
-        dp[1] = max(dp[0], nums[1]); // when at 1st house, we have option of taking either previous one or this
+        dp[1] = max(nums[0], nums[1]); // when at 1st house, we have option of taking either previous one or this
         for(int i=2;i<n;i++){
             dp[i] = max(dp[i-1], nums[i] + dp[i-2]); // constraint given
         }
